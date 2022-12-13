140feb7e577fed68b303f0cf9ae113879a2d16ac
Guillaume Flandin <guillaume.flandin@gmail.com>
2022-12-13 23:47:06 +0000

    Export segments as vtt or srt files

--- @whisper/whisper.m
+++ @whisper/whisper.m
@@ -35,10 +35,8 @@ classdef whisper < handle
         %------------------------------------------------------------------
         %-Run forward pass
         %------------------------------------------------------------------
-        function [segments,tokens] = run(this,wav,opts)
-            if nargin < 3
-                opts = struct;
-            end
+        function [segments,tokens] = run(this,wav,varargin)
+            opts = get_options(varargin);
             if ~isnumeric(wav)
                 [wav,Fs] = audioread(wav);
                 if Fs ~= 16000
@@ -63,36 +61,54 @@ classdef whisper < handle
     methods (Static)
 
         function demo(model,sound,varargin)
-            if nargin < 1, model = 'tiny.en'; end
+            if nargin < 1 || isempty(model), model = 'tiny.en'; end
             if nargin < 2, sound = 'jfk'; end
-            if nargin < 3
-                opts  = struct;
-            else
-                opts = varargin{1}; % varargin->struct
-            end
             sound     = get_sound(sound);
             hW        = whisper(model);
-            [seg,tok] = hW.run(sound,opts);
+            [seg,tok] = hW.run(sound,varargin{:});
             whisper.display_tokens(tok)
         end
 
-        function display_tokens(tokens,use_colour)
+        function display_tokens(tokens,use_colour,disp_timestamps)
             if nargin < 2, use_colour = true; end
+            if nargin < 3, disp_timestamps = false; end
+            try, isdesk = usejava('desktop'); catch, isdesk = false; end
+            if isdesk, use_colour = false; end
+            if ~isfield(tokens,'p'), use_colour = false; end
+
             cols = arrayfun(@(x)sprintf('\033[38;5;%dm',x),...
                 [196,202,208,214,220,226,190,154,118,82],'UniformOutput',false);
-            if iscell(tokens)
-                disp([tokens{:}]);
-            else
-                if ~use_colour
+            if ~use_colour
+                if ~disp_timestamps
                     disp([tokens.text]);
                 else
                     for i=1:numel(tokens)
-                        if isempty(tokens(i).text), continue; end
-                        col = max(1,round(tokens(i).p.^3 * numel(cols)));
-                        fprintf('%s%s\033[0m',cols{col},tokens(i).text);
+                        fprintf('%s',tokens(i).text);
                     end
                 end
+            else
+                for i=1:numel(tokens)
+                    if isempty(tokens(i).text), continue; end
+                    col = max(1,round(tokens(i).p.^3 * numel(cols)));
+                    fprintf('%s%s\033[0m',cols{col},tokens(i).text);
+                end
             end
+            fprintf('\n');
+        end
+
+        function save(filename,tokens)
+            [pth,nam,ext] = fileparts(filename);
+            fid = fopen(filename,'wt');
+            switch lower(ext)
+                case '.vtt'
+                    save_vtt(fid,tokens);
+                case '.srt'
+                    save_srt(fid,tokens);
+                otherwise
+                    fclose(fid);
+                    error('Unknown format.');
+            end
+            fclose(fid);
         end
 
     end
@@ -127,7 +143,7 @@ end
 function sound = get_sound(sound)
     if isnumeric(sound), return; end
     pth   = fileparts(fullfile(mfilename('fullpath')));
-    pth   = fullfile(pth,'..','sounds');
+    pth   = fullfile(pth,'..','samples');
     if ismember(sound,{'jfk'})
         sound = fullfile(pth,'..','whisper.cpp','samples',sound);
     else
@@ -138,5 +154,49 @@ function sound = get_sound(sound)
         if ~exist(sound,'file')
             error('Sound file cannot be found.');
         end
+    end
+end
+
+function opts = get_options(options)
+    opts = struct;
+    if numel(options) == 1 && isstruct(options)
+        opts = options;
+    elseif mod(numel(options),2) == 0 && iscell(options)
+        for i=1:2:numel(options)
+            opts.(options{i}) = options{i+1};
+        end
+    else
+        error('Invalid options');
+    end
+end
+
+function str = t2str(t,sep)
+    if nargin < 2, sep = ':'; end
+    ms = 10 * t;
+    hr = floor(ms / (1000 * 60 * 60));
+    ms = ms - hr * (1000 * 60 * 60);
+    mn = floor(ms / (1000 * 60));
+    ms = ms - mn * (1000 * 60);
+    s  = floor(ms / 1000);
+    ms = ms - s * 1000;
+    str = sprintf('%02d:%02d:%02d%c%03d',hr,mn,s,sep,ms);
+end
+
+function save_vtt(fid,tok)
+% WebVTT (Web Video Text Tracks): https://en.wikipedia.org/wiki/WebVTT
+    fprintf(fid,'WEBVTT\n\n');
+    for i=1:numel(tok)
+        if isempty(tok(i).text), continue; end
+        fprintf(fid,'%s --> %s\n- %s\n\n',...
+            t2str(tok(i).t0), t2str(tok(i).t1), strtrim(tok(i).text));
+    end
+end
+
+function save_srt(fid,tok)
+% SubRip Text: https://en.wikipedia.org/wiki/SubRip
+    for i=1:numel(tok)
+        if isempty(tok(i).text), continue; end
+        fprintf(fid,'%d\n%s --> %s\n%s\n\n',...
+            i, t2str(tok(i).t0,','), t2str(tok(i).t1,','), strtrim(tok(i).text));
     end
 end
--- README.md
+++ README.md
@@ -2,4 +2,28 @@
 
 [![License: MIT](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
 
-Automatic speech recognition in MATLAB/Octave based on the excellent [whisper.cpp](https://github.com/ggerganov/whisper.cpp) from [Georgi Gerganov](https://github.com/ggerganov) and [OpenAI's Whisper](https://github.com/openai/whisper).
+Automatic speech recognition in MATLAB/Octave based on the excellent [whisper.cpp](https://github.com/ggerganov/whisper.cpp) from [Georgi Gerganov](https://github.com/ggerganov) and models from [OpenAI's Whisper](https://github.com/openai/whisper).
+
+## Example
+
+```
+>> whisper.demo()
+whisper_model_load: n_vocab       = 51864
+whisper_model_load: n_audio_ctx   = 1500
+whisper_model_load: n_audio_state = 384
+whisper_model_load: n_audio_head  = 6
+whisper_model_load: n_audio_layer = 4
+whisper_model_load: n_text_ctx    = 448
+whisper_model_load: n_text_state  = 384
+whisper_model_load: n_text_head   = 6
+whisper_model_load: n_text_layer  = 4
+whisper_model_load: n_mels        = 80
+whisper_model_load: f16           = 1
+whisper_model_load: type          = 1
+whisper_model_load: adding 1607 extra tokens
+whisper_model_load: mem_required  =  390.00 MB
+whisper_model_load: ggml ctx size =   73.58 MB
+whisper_model_load: memory size   =   11.41 MB
+whisper_model_load: model size    =   73.54 MB
+```
+<font color="#FF8700"> And</font><font color="#87FF00"> so</font><font color="#FF5F00"> my</font><font color="#5FFF00"> fellow</font><font color="#D7FF00"> Americans</font><font color="#FF0000"> ask</font><font color="#FFFF00"> not</font><font color="#FFAF00"> what</font><font color="#87FF00"> your</font><font color="#5FFF00"> country</font><font color="#87FF00"> can</font><font color="#5FFF00"> do</font><font color="#AFFF00"> for</font><font color="#87FF00"> you</font><font color="#FFFF00"> ask</font><font color="#5FFF00"> what you can do for your country</font>
